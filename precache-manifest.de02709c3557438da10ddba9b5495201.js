06c1e613c23bd068af8117469ec2c9582da85e89
Lee Richmond <lee@downshift.io>
2020-01-13 16:19:27 +0000

    Updates

---
+++ precache-manifest.de02709c3557438da10ddba9b5495201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1537ad6b5c13974e76fe9e0eb5926bc4",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "0946864ee8dff575451c",
+    "url": "/github-users/static/css/main.a3d74ea0.chunk.css"
+  },
+  {
+    "revision": "a5ccef1611a66a4f39e6",
+    "url": "/github-users/static/js/2.ff835686.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/github-users/static/js/2.ff835686.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0946864ee8dff575451c",
+    "url": "/github-users/static/js/main.6f690e50.chunk.js"
+  },
+  {
+    "revision": "da1b53b11567723ea7c8",
+    "url": "/github-users/static/js/runtime-main.7911f333.js"
+  }
+]);
