4d5b9cd9fbdaf570a5ea6ea1f71de03bed0b7a04
Lee Richmond <lee@downshift.io>
2020-01-13 16:10:37 +0000

    Add ApplicationHeader component

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,6 +4,12 @@ function AppHeader() {
   return(
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <span className="navbar-brand">Github Users</span>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a href="/" className="nav-link">Home</a>
+        </li>
+      </ul>
+      
     </nav>
   );
 }
