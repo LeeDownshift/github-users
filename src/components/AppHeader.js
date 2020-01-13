9779c3d07729ab9b327273f50b73bfbc41fcefab
Lee Richmond <lee@downshift.io>
2020-01-13 10:10:13 +0000

    Create component for the Application header

---
+++ src/components/AppHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AppHeader() {
+  return(
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <span className="navbar-brand">Github Users</span>
+    </nav>
+  );
+}
+
+export default AppHeader;
+
+
