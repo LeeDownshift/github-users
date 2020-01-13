ad6ac8123e59c4cdf40a73765ebf4dc413e120a3
Lee Richmond <lee@downshift.io>
2020-01-13 10:09:02 +0000

    Start building out the visual parts of the app

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+import '../stylesheets/bootstrap.min.css';
+import UserCard from './UserCard';
+import AppHeader from './AppHeader';
+
+class App extends Component {
+  state = {
+    users: [],
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div className="App">
+        <AppHeader />
+        <main className="container mt-5">
+          <form className="form-inline col-12 mx-2 mb-5">
+            <input 
+              className="form-control col-12" 
+              type="text" 
+              placeholder="Search" 
+              aria-label="Search"
+            />
+          </form>
+        </main>
+        <div className="col-10 mx-4">
+          {users.map((user) => <UserCard user={user} />)} 
+        </div>
+        {users.length === 0 ? 'Loading...' : ''}
+      </div>
+    );
+  }
+}
+
+export default App;
