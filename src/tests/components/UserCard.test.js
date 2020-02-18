5b2607ee149700605fffcad3103c69ebc13232d2
Lee Richmond <lee@downshift.io>
2020-02-18 15:13:00 +0000

    re-oranise tests

--- src/tests/components/UserCard.test.js
+++ src/tests/components/UserCard.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import UserCard from '../components/UserCard';
+import UserCard from '../../components/UserCard';
 
 Enzyme.configure({ adapter: new Adapter() });
 
