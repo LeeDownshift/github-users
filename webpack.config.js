a8080e8de4a37b6864c77802ea49125882edf43b
Lee Richmond <lee@downshift.io>
2020-02-19 08:34:59 +0000

    update webpack to compile sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: { loader: "babel-loader" } },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
     ]
   },
   plugins: [htmlPlugin],
