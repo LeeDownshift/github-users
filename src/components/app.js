eefc1eb7231816d3839119b0dc6c7f1cba177360
Lee Richmond <lee@downshift.io>
2020-02-24 14:48:16 +0000

    clean up index js and create app.js
    app.js allows us to build the routing as well as add common elements such as headers and footers

---
+++ src/components/app.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import { Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../stylesheets/style.scss';
+import { Container } from 'react-bootstrap';
+import AppHeader from './AppHeader';
+import UserList from './Users/UserList';
+import User from './User/User';
+import PageNotFound from './PageNotFound';
+
+function App() {
+  return (
+    <Container fluid>
+      <AppHeader />
+      <Switch>
+        <Route exact path="/" component={UserList} />
+        <Route path="/:slug" component={User} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Container>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './stylesheets/style.scss';
-import UserList from './components/UserList';
-import User from './components/User';
 import configureStore from './redux/configureStore';
+import App from './components/App';
 
 const store = configureStore();
 const rootElement = document.getElementById('app');
@@ -14,10 +11,7 @@ const rootElement = document.getElementById('app');
 render(
   <Provider store={store}>
     <Router basename="/">
-      <Fragment>
-        <Route exact path="/" component={UserList} />
-        <Route path="/user/:id" component={User} />
-      </Fragment>
+      <App />
     </Router>
   </Provider>,
   rootElement
