926b6f3f01a514941b613ee04fbf8b5498b01e8f
Lee Richmond <lee@downshift.io>
2020-02-24 13:54:21 +0000

    add an fallback error page

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Alert } from 'react-bootstrap';
+
+const PageNotFound = () => {
+  return(
+    <Alert variant="danger">Oops! Page not found.</Alert>
+  );
+};
+
+export default PageNotFound;
