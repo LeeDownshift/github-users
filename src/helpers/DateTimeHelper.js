36865a3ac1c14cf784fa21b652e48586af00da24
Lee Richmond <lee@downshift.io>
2020-02-27 09:54:04 +0000

    update date time helper

--- src/helpers/DateTimeHelper.js
+++ src/helpers/DateTimeHelper.js
@@ -4,3 +4,15 @@ export const formattedDate = (date) => {
   const parsed = moment(new Date(date));
   return parsed.format('D MMM YYYY');
 } 
+
+export const formattedTime = (time) => {
+  const parsed = moment(new Date(time));
+  return parsed.format('LT');
+} 
+
+export const formattedDateTime = (dateTime) => {
+  if (moment(dateTime, moment.ISO_8601, true).isValid()) {
+    return `${formattedDate(dateTime)} at ${formattedTime(dateTime)}`;
+  }
+  return 'invalid format';
+}
--- src/tests/helpers/DateTimeHelper.test.js
+++ src/tests/helpers/DateTimeHelper.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { formattedDate } from '../../helpers/DateTimeHelper';
+import { formattedDate, formattedTime, formattedDateTime } from '../../helpers/DateTimeHelper';
 
 describe('formattedDate Helper', () => {
   describe('with a valid date', () => {
@@ -17,6 +17,35 @@ describe('formattedDate Helper', () => {
     it('should return an invvalid date error', () => {
       expect(formattedDate('')).toEqual('Invalid date');
       expect(formattedDate('Test User')).toEqual('Invalid date');
+    });
+  });
+});
+
+describe('formattedTime Helper', () => {
+  describe('with a valid Time', () => {
+    it('should return a formatted Time string', () => {
+      expect(formattedTime('2020-01-05T03:50:33Z')).toEqual('3:50 AM');
+    });
+  });
+
+  describe('with an invalid time', () => {
+    it('should return an invvalid date error', () => {
+      expect(formattedTime('03:50 AM')).toEqual('Invalid date');
+      expect(formattedTime('Test User')).toEqual('Invalid date');
+    });
+  });
+});
+
+describe('formattedDateTime Helper', () => {
+  describe('with a valid date time string', () => {
+    it('should return a formatted Time string', () => {
+      expect(formattedDateTime('2020-01-05T03:50:33Z')).toEqual('5 Jan 2020 at 3:50 AM');
+    });
+  });
+
+  describe('with an invalid date time string', () => {
+    it('should return invalid format', () => {
+      expect(formattedDateTime('blibble')).toEqual('invalid format');
     });
   });
 });
